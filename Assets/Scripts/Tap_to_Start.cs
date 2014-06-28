777d61fe97fe0194d27d81b069bc2867141339c8
Kiichi Tachibana <bodega1123@gmail.com>
2014-06-28 22:13:21 +0900

    Titleリファクタ

--- Assets/Scripts/Tap_to_Start.cs
+++ Assets/Scripts/Tap_to_Start.cs
@@ -3,10 +3,32 @@ using System.Collections;
 
 public class Tap_to_Start : MonoBehaviour {
 
+	Color color;
+
+	void Start () {
+		StartCoroutine("Blink");
+	}
+
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetButtonDown("Fire1")) {
 			Application.LoadLevel("Stage 1-1");
 		}
+
+	}
+
+	IEnumerator Blink() {
+
+		while (true) {
+			color = guiText.color;
+			color.a = 0;
+			guiText.color = color;
+			yield return new WaitForSeconds(0.5f);
+			color.a = 0.5f;
+			guiText.color = color;
+			yield return new WaitForSeconds(0.5f);
+		}
+
 	}
 }
